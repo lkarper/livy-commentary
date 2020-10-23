0d41eaad6ce4cffc35ff8094a0a69bd8856730b7
Luther Karper <luther.karper@live.com>
2020-10-23 15:15:26 -0400

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import Header from './Header/Header';
 import CommentaryHome from './CommentaryHome/CommentaryHome';
