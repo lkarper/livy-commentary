cc07a4700aa40044670d30bcef4f720c4ca0e15d
Luther Karper <luther@Luthers-Air.cable.rcn.com>
2020-11-10 19:59:41 -0500

    Update CommentaryReadChapter component
    Add propTypes and defaultProps to CommentaryReadChapter.

--- src/CommentaryRead/CommentaryReadChapter/CommentaryReadChapter.js
+++ src/CommentaryRead/CommentaryReadChapter/CommentaryReadChapter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import CommentaryReadSectionView from '../CommentaryReadSectionView/CommentaryReadSectionView';
 
 const CommentaryReadChapter = (props) => {
@@ -17,5 +18,23 @@ const CommentaryReadChapter = (props) => {
        </>
    );
 }
+
+CommentaryReadChapter.defaultProps = {
+    chapter: {
+        chapter_number: '',
+        chapter_intro: '',
+        chapter_title: '',
+        sections: [],
+    },
+};
+
+CommentaryReadChapter.propTypes = {
+    chapter: PropTypes.shape({
+        chapter_number: PropTypes.string,
+        chapter_intro: PropTypes.string,
+        chapter_title: PropTypes.string,
+        sections: PropTypes.arrayOf(PropTypes.object),
+    }),
+};
 
 export default CommentaryReadChapter;
