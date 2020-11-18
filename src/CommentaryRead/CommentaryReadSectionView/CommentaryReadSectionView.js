c558b924eb55aa396ff74747cd28699611092a04
Luther Karper <luther@Luthers-Air.cable.rcn.com>
2020-11-18 18:31:14 -0500

    Update CommentaryReadSectionView component
    Add propTypes and defaultProps to CommentaryReadSectionView.

--- src/CommentaryRead/CommentaryReadSectionView/CommentaryReadSectionView.js
+++ src/CommentaryRead/CommentaryReadSectionView/CommentaryReadSectionView.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import CommentaryReadComment from '../CommentaryReadComment/CommentaryReadComment';
 import './CommentaryReadSectionView.css';
 
 const CommentaryReadSectionView = (props) => {
 
     const { section } = props;
+
+    if (!section.section_number || !section.latin) {
+        return (
+            <div className="CommentaryReadSectionView__container">
+                <h3>Error</h3>
+                <p>Looks like something went wrong; check your connection and try again.</p>
+            </div>
+        );
+    }
 
     return (
         <div className='CommentaryReadSectionView__container'>
@@ -14,7 +24,7 @@ const CommentaryReadSectionView = (props) => {
             </section>
             <section className='CommentaryReadSectionView__section'>
                 <h3>Commentary and Notes</h3>
-                {section.comments.length !==0 && 
+                {section.comments.length !== 0 && 
                     section.comments
                         .sort((a, b) => a.comment_order - b.comment_order)
                         .map((comment, i) => <CommentaryReadComment key={i} comment={comment} />)}
@@ -23,5 +33,22 @@ const CommentaryReadSectionView = (props) => {
         </div>
     );
 }
+
+CommentaryReadSectionView.defaultProps = {
+    section: {
+        section_number: '',
+        latin: '',
+        comments: [],
+    },
+};
+
+CommentaryReadSectionView.propTypes = {
+    section: PropTypes.shape({
+        section_number: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        latin: PropTypes.string,
+        comments: PropTypes.arrayOf(PropTypes.object),
+    }),
+}
+
 
 export default CommentaryReadSectionView;
